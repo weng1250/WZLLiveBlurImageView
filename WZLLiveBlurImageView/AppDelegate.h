30fecae83411da37940ad785de04b88e9c27bf13
weng1250 <719113951@qq.com>
2015-03-23 16:33:34 +0800

    initial commit

---
+++ WZLLiveBlurImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WZLLiveBlurImageView
+//
+//  Created by zilin_weng on 15/3/23.
+//  Copyright (c) 2015年 Weng-Zilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
